af566a656fe5cf1ff844f717d0df37b0eb3825ee
saeedehghanadbashi <saeedeh.ghanadbashi@ucdconnect.ie>
2021-12-18 17:31:38 +0330

    action masking with server limit + observation masking for user card number and user device info have been implemented

---
+++ src/observation_masking/user_device_info/baseline/run_this.py
@@ -0,0 +1,159 @@
+from env import Env
+from DDPG import DDPG
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import time
+
+#####################  hyper parameters  ####################
+CHECK_EPISODE = 4
+LEARNING_MAX_EPISODE = 10
+MAX_EP_STEPS = 1500 #600 #3000
+TEXT_RENDER = False
+SCREEN_RENDER = False
+CHANGE = False
+SLEEP_TIME = 0.1
+
+#####################  function  ####################
+def exploration (a, r_dim, b_dim, r_var, b_var):
+    for i in range(r_dim + b_dim):
+        # resource
+        if i < r_dim:
+            a[i] = np.clip(np.random.normal(a[i], r_var), 0, 1) * r_bound
+        # bandwidth
+        elif i < r_dim + b_dim:
+            a[i] = np.clip(np.random.normal(a[i], b_var), 0, 1) * b_bound
+    return a
+
+###############################  training  ####################################
+
+if __name__ == "__main__":
+    env = Env()
+    s_dim, r_dim, b_dim, o_dim, r_bound, b_bound, task_inf, limit, location, MAX_REQ_TIMER, ALGORITHM, METHOD, CONCEPT, SERVER_LIMIT_RANGE, LATENCY_REQUIREMENTS = env.get_inf()
+    ddpg = DDPG(s_dim, r_dim, b_dim, o_dim, r_bound, b_bound)
+
+    r_var = 1  # control exploration
+    b_var = 1
+    ep_reward = []
+    ep_tasks_without_delay = []
+    ep_tasks_with_delay = []
+    ep_tasks_failed_due_delay = []   
+    r_v, b_v = [], []
+    var_reward = []
+    max_rewards = 0
+    episode = 0
+    var_counter = 0
+    epoch_inf = []
+    while var_counter < LEARNING_MAX_EPISODE  and episode <=19:
+        print("Learning step is: ", var_counter, "/", LEARNING_MAX_EPISODE)
+        #print("episode: ", episode)
+        # initialize
+        s = env.reset()
+        ep_reward.append(0)
+        ep_tasks_without_delay.append(0)
+        ep_tasks_with_delay.append(0)
+        ep_tasks_failed_due_delay.append(0)
+        if SCREEN_RENDER:
+            env.initial_screen_demo()
+
+        for j in range(MAX_EP_STEPS):
+            time.sleep(SLEEP_TIME)
+            # render
+            if SCREEN_RENDER:
+                env.screen_demo()
+            if TEXT_RENDER and j % 30 == 0:
+                env.text_render()
+
+            # DDPG
+            # choose action according to state
+            a = ddpg.choose_action(s)  # a = [R B O]
+            # add randomness to action selection for exploration
+            a = exploration(a, r_dim, b_dim, r_var, b_var)
+            # store the transition parameter
+            s_, r,  tasks_without_delay, tasks_with_delay, tasks_failed_due_delay = env.ddpg_step_forward(a, r_dim, b_dim)
+            ddpg.store_transition(s, a, r / 10, s_)
+            # learn
+            if ddpg.pointer == ddpg.memory_capacity:
+                print("start learning")
+            if ddpg.pointer > ddpg.memory_capacity:
+                ddpg.learn()
+                if CHANGE:
+                    r_var *= .99999
+                    b_var *= .99999
+            # replace the state
+            s = s_
+            # sum up the reward
+            ep_reward[episode] += r
+            ep_tasks_without_delay[episode] += tasks_without_delay
+            ep_tasks_with_delay[episode] += tasks_with_delay
+            ep_tasks_failed_due_delay[episode] += tasks_failed_due_delay
+            # in the end of the episode
+            if j == MAX_EP_STEPS - 1:
+                var_reward.append(ep_reward[episode])
+                r_v.append(r_var)
+                b_v.append(b_var)
+                print('Episode:%3d' % episode, ' Reward: %5d' % ep_reward[episode], ' Tasks without delay: %5d' % ep_tasks_without_delay[episode], ' Tasks with delay: %5d' % ep_tasks_with_delay[episode], ' Tasks failed due delay: %5d' % ep_tasks_failed_due_delay[episode], '###  r_var: %.2f ' % r_var,'b_var: %.2f ' % b_var, )
+                string = 'Episode:%3d' % episode + ' Reward: %5d' % ep_reward[episode] + ' Tasks without delay: %5d' % ep_tasks_without_delay[episode] + ' Tasks with delay: %5d' % ep_tasks_with_delay[episode] + ' Tasks failed due delay: %5d' % ep_tasks_failed_due_delay[episode] + '###  r_var: %.2f ' % r_var + 'b_var: %.2f ' % b_var
+                epoch_inf.append(string)
+                # variation change
+                if var_counter >= CHECK_EPISODE and np.mean(var_reward[-CHECK_EPISODE:]) >= max_rewards:
+                    CHANGE = True
+                    var_counter = 0
+                    max_rewards = np.mean(var_reward[-CHECK_EPISODE:])
+                    var_reward = []
+                else:
+                    CHANGE = False
+                    var_counter += 1
+
+        # end the episode
+        if SCREEN_RENDER:
+            env.canvas.tk.destroy()
+        episode += 1
+
+    # make directory
+    dir_name = 'output/' + 'ddpg_' + ALGORITHM + '_' + METHOD + '_' + 'with concept_' + CONCEPT + '_' + 'with server limit range_'+ SERVER_LIMIT_RANGE +  '_with latency requirements_' + LATENCY_REQUIREMENTS + '_' + str(MAX_REQ_TIMER) + 'MRT' +str(r_dim) + 'u' + str(int(o_dim / r_dim)) + 'e' + str(limit) + 'l' + location + '_for ' + str(MAX_EP_STEPS) + 'steps'
+    if (os.path.isdir(dir_name)):
+        os.rmdir(dir_name)
+    os.makedirs(dir_name)
+    # plot the reward
+    fig_reward = plt.figure()
+    plt.plot([i+1 for i in range(episode)], ep_reward)
+    plt.xlabel("episode")
+    plt.ylabel("rewards")
+    fig_reward.savefig(dir_name + '/rewards.png')
+    # plot the variance
+    fig_variance = plt.figure()
+    plt.plot([i + 1 for i in range(episode)], r_v, b_v)
+    plt.xlabel("episode")
+    plt.ylabel("variance")
+    fig_variance.savefig(dir_name + '/variance.png')
+    # write the record
+    f = open(dir_name + '/record.txt', 'a')
+    f.write('time(s):' + str(MAX_EP_STEPS) + '\n\n')
+    f.write('user_number:' + str(r_dim) + '\n\n')
+    f.write('edge_number:' + str(int(o_dim / r_dim)) + '\n\n')
+    f.write('limit:' + str(limit) + '\n\n')
+    f.write('task information:' + '\n')
+    f.write(task_inf + '\n\n')
+    for i in range(episode):
+        f.write(epoch_inf[i] + '\n')
+    # mean of the rewards
+    print("the mean of the rewards in the last", LEARNING_MAX_EPISODE, " epochs:", str(np.mean(ep_reward[-LEARNING_MAX_EPISODE:])))
+    f.write("the mean of the rewards:" + str(np.mean(ep_reward[-LEARNING_MAX_EPISODE:])) + '\n\n')
+    # mean of the tasks without delay
+    print("the mean of the tasks without delay in the last", LEARNING_MAX_EPISODE, " epochs:", str(np.mean(ep_tasks_without_delay[-LEARNING_MAX_EPISODE:])))
+    f.write("the mean of the tasks without delay:" + str(np.mean(ep_tasks_without_delay[-LEARNING_MAX_EPISODE:])) + '\n\n')
+    # mean of the tasks with delay
+    print("the mean of the tasks with delay in the last", LEARNING_MAX_EPISODE, " epochs:", str(np.mean(ep_tasks_with_delay[-LEARNING_MAX_EPISODE:])))
+    f.write("the mean of the tasks with delay:" + str(np.mean(ep_tasks_with_delay[-LEARNING_MAX_EPISODE:])) + '\n\n')
+    # mean of the tasks failed due to the delay
+    print("the mean of the tasks failed due to the delay in the last", LEARNING_MAX_EPISODE, " epochs:", str(np.mean(ep_tasks_failed_due_delay[-LEARNING_MAX_EPISODE:])))
+    f.write("the mean of the tasks failed due to the delay:" + str(np.mean(ep_tasks_failed_due_delay[-LEARNING_MAX_EPISODE:])) + '\n\n')
+    # standard deviation
+    print("the standard deviation of the rewards:", str(np.std(ep_reward[-LEARNING_MAX_EPISODE:])))
+    f.write("the standard deviation of the rewards:" + str(np.std(ep_reward[-LEARNING_MAX_EPISODE:])) + '\n\n')
+    # range
+    print("the range of the rewards:", str(max(ep_reward[-LEARNING_MAX_EPISODE:]) - min(ep_reward[-LEARNING_MAX_EPISODE:])))
+    f.write("the range of the rewards:" + str(max(ep_reward[-LEARNING_MAX_EPISODE:]) - min(ep_reward[-LEARNING_MAX_EPISODE:])) + '\n\n')
+    f.close()
+
