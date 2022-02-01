78e51e0fa28c69b0700ce4de2c83ad26d9ebf07d
saeedehghanadbashi <saeedeh.ghanadbashi@ucdconnect.ie>
2022-02-01 10:42:11 +0330

    results have been added

--- hard-25/run_bash.sh
+++ hard-25/run_bash.sh
@@ -2,8 +2,6 @@
 set -x #echo on
 #!/bin/bash -xv
 PS4='${LINENO}: '
-python3 src/execution_prioritization_based_on_application_type/proposed_method/run_this.py
-python3 src/execution_prioritization_based_on_application_type/proposed_method/run_this.py
 python3 src/observation_masking/user_card_number/baseline/run_this.py
 python3 src/observation_masking/user_card_number/proposed_method/run_this.py
 python3 src/observation_masking/user_card_number/baseline/run_this.py
@@ -13,14 +11,4 @@ python3 src/observation_masking/user_card_number/proposed_method/run_this.py
 python3 src/observation_masking/user_card_number/baseline/run_this.py
 python3 src/observation_masking/user_card_number/proposed_method/run_this.py
 python3 src/observation_masking/user_card_number/baseline/run_this.py
-python3 src/observation_masking/user_card_number/proposed_method/run_this.py
 python3 src/observation_masking/user_card_number/baseline/run_this.py
-python3 src/observation_masking/user_card_number/proposed_method/run_this.py
-python3 src/observation_masking/user_card_number/baseline/run_this.py
-python3 src/observation_masking/user_card_number/proposed_method/run_this.py
-python3 src/observation_masking/user_card_number/baseline/run_this.py
-python3 src/observation_masking/user_card_number/proposed_method/run_this.py
-python3 src/observation_masking/user_card_number/baseline/run_this.py
-python3 src/observation_masking/user_card_number/proposed_method/run_this.py
-python3 src/observation_masking/user_card_number/baseline/run_this.py
-python3 src/observation_masking/user_card_number/proposed_method/run_this.py
